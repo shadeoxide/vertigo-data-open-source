09e3c8464b2ee08753afb979dfdae0693ec9f7cb
1337 <33544674+conver4y@users.noreply.github.com>
2020-11-15 14:16:59 +0800

    Create blacklisted_discord_server_type.js

---
+++ Atrium 3: Sexual Activity/blacklisted_discord_server_type.js
@@ -0,0 +1,14 @@
+let serverList = [
+	"gaygeeks", "pornhub", "xvideos", "xnxx", 
+	"nhentai", "hentaihaven", "nekopoi", "18", "sex"
+];
+
+let wordList = [
+	"gay", "18", "18+", "hentai", "lewd", "bdsm", "kink", "ass", "a.s.s", 
+	"nsfw", "69", "r34", "e621", "porn", "xxx", "adult",
+	"sex", "s3x", "g4y", "h3ntai"
+];
+
+module.exports = [serverList, wordList];
+
+// We usually use this to check the server details.
